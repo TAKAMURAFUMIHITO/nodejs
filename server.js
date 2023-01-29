1a7ff0d64caeff6ab463e5c990dd563453a6979d
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-01-30 02:00:48 +0900

    add http

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('OK.');
+// http: サーバーとクライアントとのやり取りをすることができる
+const http = require('http');
+const PORT = 8000;
+const html = require("fs").readFileSync("./index.html");
+
+const server = http.createServer((req, res) => {
+  // ヘッダー情報を返す
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.write(html);
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log("server running!");
+});
